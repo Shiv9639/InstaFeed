44f04ce5c78eb36e728671243c5540a73472acc6
Shiv9639 <passawalashivang@gmail.com>
2021-03-28 13:16:00 -0400

    added apis

---
+++ express_mongo/routes/api.js
@@ -0,0 +1,60 @@
+var express = require("express");
+var router = express.Router();
+var postService = require("../services/postService.js");
+
+// ********************* //
+// base api call: '/api' //
+// ********************* //
+
+router.get("/getPost/:id/:updateViewCount", function (req, res) {
+  return postService
+    .getPostById(req.params.id, req.params.updateViewCount)
+    .then((data) => {
+      res.json(data);
+    });
+});
+
+router.post("/submitPost", function (req, res) {
+  return postService.submitPost(req.body).then((data) => {
+    res.json(data);
+  });
+});
+
+// **** Syntax ****
+// {
+//   "postId": <Post Id>
+//   "comment": "<Your comment>"
+// }
+// ****************
+router.post("/submitComment", function (req, res) {
+  return postService.submitCommentOnPost(req.body).then((data) => {
+    res.json(data);
+  });
+});
+
+// **** Syntax ****
+// {
+//   "category": "<Your category>"
+//   "sort": "<Your sorting query>"
+//   "tag": "<Your tag>"
+// }
+// Tags must be in lowercase
+// ****************
+router.post("/getFeed", function (req, res) {
+  return postService.getFeed(req.body).then((data) => {
+    res.json(data);
+  });
+});
+
+router.post("/updateLikes/:id", function (req, res) {
+  return postService.incrementLikeOnPost(req.params.id).then((data) => {
+    res.json(data);
+  });
+});
+router.delete("/deletePost/:postId", function (req, res) {
+  return postService.deletePostbyId(req.params.postId).then((data) => {
+    res.json(data);
+  });
+});
+
+module.exports = router;
