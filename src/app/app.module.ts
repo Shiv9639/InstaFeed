4657b971659499026969e60eac550a49fb48f1ed
vishwesh-97 <71589834+vishwesh-97@users.noreply.github.com>
2021-03-28 22:51:42 +0530

    added app component module

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FeedComponent } from "./feed/feed.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { NewPostComponent } from "./new-post/new-post.component";
+import { PostComponent } from "./post/post.component";
+import { CommonModule } from "@angular/common";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LoadingComponent } from './loading/loading.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FeedComponent,
+    NavbarComponent,
+    NewPostComponent,
+    PostComponent,
+    LoadingComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
